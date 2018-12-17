4e8a7184e0ee00b05b9af09c0a19cc5aa33e9dfb
Vasiliy Makarov <drmoriarty.0@gmail.com>
2018-12-17 12:16:16 +0300

    update libraries

--- app/jni/firebase/Android.mk
+++ app/jni/firebase/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := firebase
 ifeq ($(APP_STL), c++_static)
-    LOCAL_SRC_FILES := libs_c++_static/$(TARGET_ARCH_ABI)/libapp.a
+    LOCAL_SRC_FILES := libs_c++_static/$(TARGET_ARCH_ABI)/libfirebase_app.a
 else
-    LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libapp.a
+    LOCAL_SRC_FILES := libs/$(TARGET_ARCH_ABI)/libfirebase_app.a
 endif
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
 include $(PREBUILT_STATIC_LIBRARY)
